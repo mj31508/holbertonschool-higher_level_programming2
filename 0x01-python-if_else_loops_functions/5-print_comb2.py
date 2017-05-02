7638dfa31726be1e0078420c11fb42a640b8af0f
Max <mj31508@gmail.com>
2017-05-02 19:40:41 -0400

    counting up to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 # print 2 digits numbers from 00 to 99
 a = 0
-while a <= 99:
+while a <= 98:
     print("{:02d}, ".format(a), end="")
     a += 1
-    if a == 99:
-        print("{:02d}".format(a))
-        break
+print("99")
