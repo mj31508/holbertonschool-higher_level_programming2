8c9333f4606af96c91631afb100c1e54613875d6
Max <mj31508@gmail.com>
2017-05-03 01:52:22 -0400

    fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ s = ("Last digit of")
 s2 = ("and is greater than 5")
 s3 = ("and is 0")
 s4 = ("and is less than 6 and not 0")
-if number > 0:
+if number >= 0:
     last = number % 10
 if number < 0:
     last = number % -10
