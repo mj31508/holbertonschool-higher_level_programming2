99bab3e095580f610758870ae2c87ec323511640
Max <mj31508@gmail.com>
2017-05-05 22:35:28 -0400

    bad variable name

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__=="__main__":
     import sys
     args = len(sys.argv)
     if args == 1:
-        print("{} argument.".format(argc - 1))
+        print("{} argument.".format(args - 1))
     elif args == 2:
-        print("{} argumrnt:".format(argc - 1))
+        print("{} argumrnt:".format(args - 1))
         print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{} arguments:".format(argc - 1))
+        print("{} arguments:".format(args - 1))
         for i in rage(1, args):
             print("{}: {}".format(i, sys.argv[i]))
