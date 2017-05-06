a46806ad5f3b2d23f6c06c762a8ef8d4bbe92710
Max <mj31508@gmail.com>
2017-05-05 22:38:27 -0400

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__=="__main__":
     if args == 1:
         print("{} argument.".format(args - 1))
     elif args == 2:
-        print("{} argument:".format(args - 1))
+        print("1 argument:".format(args - 1))
         print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{} arguments:".format(args - 1))
+        print("{:d} arguments:".format(args - 1))
         for i in rage(1, args):
             print("{}: {}".format(i, sys.argv[i]))
