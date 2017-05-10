42492264ac9be8464ad90354048cd848f719f58c
Max <mj31508@gmail.com>
2017-05-10 10:07:59 -0400

    Remove two capital characters from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string[:]
-    for i in new_string:
-        if i == chr(67) or i == chr(99):
-            new_string = "" + new_string[1:]
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != chr(67) and my_string[i] != chr(99):
+            new_string += my_string[i]
     return new_string
