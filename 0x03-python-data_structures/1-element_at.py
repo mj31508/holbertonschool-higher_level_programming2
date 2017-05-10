d1dabb3c02cdaabb72ca65aa571e70ad4a925bac
Max <mj31508@gmail.com>
2017-05-10 00:31:17 -0400

    fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list is None:
-        return my_list
-    
-    for i in range(len(my_list)):
         if idx >= len(my_list)-1 or idx <= 0:
             return None
         else:
-            return("{}".format(my_list[idx]))
+            return my_list[idx]
