dbc31fb1a9af3bb3683b72d010bcc3da559df602
Max <mj31508@gmail.com>
2017-05-09 22:00:28 -0400

    replace a element at any position

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx > len(my_list) or idx < 0:
-        new_list = my_list[:]
-        return new_list
+    if my_list is None:
+        return my_list
+
+    if idx < 0 or idx >= len(my_list):
+        copied_list = my_list[:]
+        return copied_list
 
     new_list = my_list[:]
-    del new_list[3]
-    new_list.insert(3, element)
+    new_list[idx] = element
     return new_list
