51d02d7dc49a7ca6e4ae669cb8098c2d63c9d98a
Max <mj31508@gmail.com>
2017-05-12 17:16:06 -0400

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    if my_list is None or x <= 0 or my_list == []:
+        print()
+        return None
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+        print()
+    except IndexError:
+        print()
+        return(i)
+    return(i+1)
