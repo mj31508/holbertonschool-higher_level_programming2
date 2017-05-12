b9561212373f00d77bfdb9c1bbe0da9883eb0ab9
Max <mj31508@gmail.com>
2017-05-11 22:26:40 -0400

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None:
+    if my_dict is None or my_dict == {}:
         return None
     return max(my_dict, key=lambda i: my_dict[i])
