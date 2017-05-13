509beb429cf5e7e905615244938d8c817aa7561b
Max <mj31508@gmail.com>
2017-05-13 00:05:54 -0400

    fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    print("Inside Result: ", end="")
     try:
         total=a/b
     except (ZeroDivisionError) as error:
         total = None
 
     finally:
-        print("Inside Result: {}".format(total))
+        print("{}".format(total))
     return(total)
