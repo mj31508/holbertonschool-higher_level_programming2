a8b58b3e9b57161290adcc3dc0de2815a99a905d
Max <mj31508@gmail.com>
2017-05-13 00:12:17 -0400

    change

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    print("Inside Result: ", end="")
     try:
         total=a/b
     except:
         total = None
 
     finally:
+        print("Inside Result: ", end="")
         print("{}".format(total))
     return(total)
