96eae1e6fa2abb8fb6310a876604fb2bb6a115fc
Max <mj31508@gmail.com>
2017-05-23 13:07:10 -0400

    pep8 fix

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -8,8 +8,7 @@ class Square():
     def __init__(self, size=0):
         self.__size = size
 
-
-    @property
+        @property
     def size(self):
         return self.__size
 
@@ -17,10 +16,10 @@ class Square():
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif (value < 0):
+        elif value < 0:
             raise ValueError("Size must be >= 0")
         else:
             self.__size = value
 
     def area(self):
-        return (self.__size ** 2)
+        return self.__size ** 2
