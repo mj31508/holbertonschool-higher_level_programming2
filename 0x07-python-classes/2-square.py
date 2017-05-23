07d4e19e32b7214ed8cb30150bd18319ccbdfba4
Max <mj31508@gmail.com>
2017-05-23 03:02:32 -0400

    fix

--- 0x07-python-classes/2-square.py
+++ 0x07-python-classes/2-square.py
@@ -10,4 +10,4 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size """the use of __ makes size unchangable"""
+        self.__size = size
