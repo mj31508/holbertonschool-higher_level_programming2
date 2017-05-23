1e2b0e696070abedad7a92518348de0020f3e13f
mj31508 <max.johnson@holbertonschool.com>
2017-05-23 21:15:36 +0000

    Hopeful fix

--- 0x07-python-classes/1-square.py
+++ 0x07-python-classes/1-square.py
@@ -5,5 +5,5 @@ Defines a empty class Square
 
 
 class Square():
-    def __init__(self, size):  """__init__ makes it private"""
-    self.__size = size
+    def __init__(self, size):
+        self.__size = size
