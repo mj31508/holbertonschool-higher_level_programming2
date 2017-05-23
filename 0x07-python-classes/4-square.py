7d7d5e2090974db046db294143278d39d219a9ca
mj31508 <max.johnson@holbertonschool.com>
2017-05-23 17:56:54 +0000

    fix

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -6,7 +6,12 @@ Defines class square
 
 class Square():
     def __init__(self, size=0):
-        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
