11f2e1b2a45994d644a131825afc1b84d5506bbb
Max <mj31508@gmail.com>
2017-05-24 02:23:37 -0400

    commits after pep8 checks, still not the right output

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Define a Rectangle Class
+"""
+
+
+class Rectangle:
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >=0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >=0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return(0)
+        else:
+            return (self.__width + self.__width + self.height + self.height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return("")
+        else:
+            rectangle = "#"
+            for i in range(self.__height):
+                for j in range(self.height - 1):
+                    print(rectangle * self.__width)
+                return(rectangle * self.width)
+
+    def __repr__(self):
+        return("Rectangle{}, {}".format(self.width, self.height))
