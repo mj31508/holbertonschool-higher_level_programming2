b94b2ee8b718d7b23a13807845ce0de0930a2bad
Max <mj31508@gmail.com>
2017-06-01 21:40:29 -0400

    removal of if statements and fixed tabs

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         name = "width"
-        super().integer_validator(name, width):
-            self.__width = width
-        super().integer_validator(name, height):
-            self.__height = height
+        super().integer_validator(name, width)
+        super().integer_validator(name, height)
+        self.__height = height
+        self.__width = width
