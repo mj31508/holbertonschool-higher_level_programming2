9137ab7ffa2a36867ca29b21abc4a24f85b517a8
Max <mj31508@gmail.com>
2017-06-01 20:01:15 -0400

    edit of 2 project

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@ using isinstance to fnd same type
 
 
 def is_same_class(obj, a_class):
-    if isinstance(obj, a_class):
-        return isinstance(obj, a_class)
+    if type(obj) is a_class:
+        return True
     else:
         return False
