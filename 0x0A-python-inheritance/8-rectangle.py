829858067e36818eb5d8b1cdd5aa38f91e9bf0e9
Max <mj31508@gmail.com>
2017-06-01 21:38:07 -0400

    removed if  statement

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,10 +4,11 @@ raising an exception
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         name = "width"
-        if super().integer_validator(name, width):
+        super().integer_validator(name, width):
             self.__width = width
-        if super().integer_validator(name, height):
+        super().integer_validator(name, height):
             self.__height = height
