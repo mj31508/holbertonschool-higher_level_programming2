8b911e50820bf9637ddf00e4f113f6962211d449
Max <mj31508@gmail.com>
2017-06-03 02:47:54 -0400

    removal of encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ Read a text file
 
 
 def read_file(filename=""):
-    with open("my_file_0.txt", "r", encoding="utf-8") as filename:
+    with open("my_file_0.txt", "r") as filename:
         for line in filename:
             print(line.strip('\n'))
