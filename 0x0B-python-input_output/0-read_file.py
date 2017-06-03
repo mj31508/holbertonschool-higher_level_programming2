fe8f485d27ac2214cb61f6f71eff35ebdcb31f14
Max <mj31508@gmail.com>
2017-06-03 02:57:20 -0400

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,5 @@ Read a text file
 
 
 def read_file(filename=""):
-    with open("my_file_0.txt", "r") as filename:
-        for line in filename:
-            print(line.strip('\n'))
+    with open(filename, "r", encoding='utf-8') as filename:
+            print(filename.read(),'\n')
