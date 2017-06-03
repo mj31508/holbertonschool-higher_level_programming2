c8089f5ad1e26abb1a3bc918442606331f14ef6b
Max <mj31508@gmail.com>
2017-06-03 02:30:35 -0400

    change

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,4 @@ print sorted list using the sort()
 
 class MyList(list):
     def print_sorted(self):
-        for i in self:
-            if isinstance(i, int) is False:
-                print(sorted(self))
+        print(sorted(self))
