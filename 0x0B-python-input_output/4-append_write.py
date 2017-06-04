c4853323fa60eef4c04e3f8b22cf6d2fe8f96e2e
Max <mj31508@gmail.com>
2017-06-03 21:18:53 -0400

    append

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+appending a string to text file
+"""
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as filename:
+        filename.write(text)
+        return(len(text))
