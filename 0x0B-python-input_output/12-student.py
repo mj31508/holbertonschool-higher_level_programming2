c46a060f789fd6d4b1c91e0eedcb26cf7b500014
Max <mj31508@gmail.com>
2017-06-04 13:52:06 -0400

    hopeful

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defining a class
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self, attrs=None):
+            if attrs is None:
+                return self.__dict__
+            else:
+                dict1 = {}
+                for attr in attrs:
+                    if hasattr(self, attr):
+                        dict1[attr] = getattr(self, attr)
+                return dict1
