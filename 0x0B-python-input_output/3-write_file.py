ae6b0eaffcaddf3ef4fd1d266aae3fa7d9cf52c1
Max <mj31508@gmail.com>
2017-06-03 21:17:38 -0400

    write a string to text

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -5,6 +5,6 @@ write a string to text file
 
 
 def write_file(filename="", text=""):
-    with open(filename,"w", encoding="utf-8") as filename:
+    with open(filename, "w", encoding="utf-8") as filename:
         total = filename.write(text)
         return total
