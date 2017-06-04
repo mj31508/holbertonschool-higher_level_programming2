b6809524b1a1882ca0d2237f5110d53f0fbeab44
Max <mj31508@gmail.com>
2017-06-03 21:14:10 -0400

    change

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,7 +6,7 @@ read n lines of txt file
 
 def read_lines(filename="", nb_lines=0):
     line_count = 0
-    with open("my_file_0.txt", "r", encoding="utf-8") as filename:
+    with open(filename, "r", encoding="utf-8") as filename:
         for line in filename:
             line_count += 1
             if nb_lines <= 0 or nb_lines >= line_count:
