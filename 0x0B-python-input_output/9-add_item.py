f364610f8789be1555c44ce5f5488cd4f4b6121b
Max <mj31508@gmail.com>
2017-06-04 02:52:05 -0400

    file fix

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,11 +10,12 @@ import os.path
 save_from_json = __import__('7-save_to_json_file').save_to_json_file
 load_from_json = __import__('8-load_from_json_file').load_from_json_file
 
-MyList = []
+my_list = []
+
 if not os.path.exists("./add_item.json"):
-    savefromjson(MyList, "add_item.json")
-MyList = load_from_json("add_item.json")
+        save_from_json(my_list, "add_item.json")
+my_list = load_from_json("add_item.json")
 
 for i in sys.argv[1:]:
-    MyList.append(i)
-savefromjson(MyList, "add_item.json")
+    my_list.append(i)
+save_from_json(my_list, "add_item.json")
