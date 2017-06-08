a846053e4fa6eaa80bf373893299711bee545dcd
Max <mj31508@gmail.com>
2017-06-08 13:21:02 -0400

    recommitting test cases

--- 0x06-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x06-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,35 +13,43 @@ class TestMaxInteger(unittest.TestCase):
         print("test_isint")
         self.assertTrue(max_integer([a, 2]) == 4)
 
+    def test_multi_num(self):
+        a = 50
+        self.assertEqual(max_integer([a,a,a,a]), a)
+
+    def test_unsorted_num(self):
+        self.assertEqual(max_integer([1,4,2,2]), 4)
+
     def test_maxnum(self):
         print("test_maxnum")
         self.assertEqual(max_integer([1, 12, 4]), 12)
 
-    def num_at_end(self):
+    def test_num_at_end(self):
+        a = 4
         print("test num at end")
-        self.assertTrue(max_integer([1,2,3,4]) == 4)
+        self.assertEqual(max_integer([1,2,3,a]), a)
 
     def test_float(self):
         print("test_float")
         self.assertEqual(max_integer([4.0]), 4.0)
 
-    def first_num(self):
-        self.assertEqual(max_integer([4,3,2,1]))
+    def test_first_num(self):
+        self.assertEqual(max_integer([4,3,2,1]), 4)
 
-    def mid_max(self):
+    def test_mid_max(self):
         self.assertEqual(max_integer([1,2,4,3]), 4)
 
-    def neg_number(self):
+    def test_neg_number(self):
         self.assertTrue(max_integer([1,2,-3,4]), 4)
 
-    def only_neg(self):
+    def test_only_neg(self):
         self.assertTrue(max_integer([-1,-2,-3,-4]))
 
-    def one_num(self):
+    def test_one_num(self):
         self.assertTrue(max_integer([4]))
 
-    def is_empty(self):
-        self.assertTrue(max_integer([]))
+    def test_is_empty(self):
+        self.assertIsNone(max_integer([]))
 
 if __name__ == '__main__':
         unittest.main()
