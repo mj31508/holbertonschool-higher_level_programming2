f37e13fc52faa1d4a10cb084333ff35c3353b0f5
Max <mj31508@gmail.com>
2017-06-08 12:53:00 -0400

    added tests

--- 0x06-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x06-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,66 +1,47 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+Module to find the max integer
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
 
-    def test_with_ints(self):
-        self.assertEqual(max_integer([1,2,3,4]),4)
+    def test_isint(self):
+        a =  4
+        print("test_isint")
+        self.assertTrue(max_integer([a, 2]) == 4)
+
+    def test_maxnum(self):
+        print("test_maxnum")
+        self.assertEqual(max_integer([1, 12, 4]), 12)
+
+    def num_at_end(self):
+        print("test num at end")
+        self.assertTrue(max_integer([1,2,3,4]) == 4)
 
     def test_float(self):
-        self.assertEqual(max_integer([1,2.0,3,4],2.0)
+        print("test_float")
+        self.assertEqual(max_integer([4.0]), 4.0)
 
-    def test_large_int(self):
-        self.assertTrueEqual(max_integer([54085308],54085308)
+    def first_num(self):
+        self.assertEqual(max_integer([4,3,2,1]))
 
-    def test_bool(self):
-        self.assertEqual(max_integer([True, False, 3, 4], 4)
+    def mid_max(self):
+        self.assertEqual(max_integer([1,2,4,3]), 4)
 
-     def test_list_of_string(self):
-        self.assertEqual(max_integer(["Holby"]), "Holby")
+    def neg_number(self):
+        self.assertTrue(max_integer([1,2,-3,4]), 4)
 
-    def test_max_string(self):
-        self.assertEqual(max_integer("Hakeem"), 'm')
+    def only_neg(self):
+        self.assertTrue(max_integer([-1,-2,-3,-4]))
 
-    def test_bool_list(self):
-        self.assertEqual(max_integer([True, False, 3, 4]), 4)
+    def one_num(self):
+        self.assertTrue(max_integer([4]))
 
-    def test_negatives(self):
-        self.assertEqual(max_integer([-5, -1, -19, 2]), 2)
+    def is_empty(self):
+        self.assertTrue(max_integer([]))
 
-    def test)none(self):
-        self.assertEqual(max_integer([None]), None)
-
-    @unittest.expectedFailure
-    def test_c_list(self):
-        self.assertEqual(max_integer(c), NameError)
-
-    @unittest.expectedFailure
-    def test_none_at_all(self):
-        self.assertEqual(max_integer([None, None, None, None]), TypeError)
-
-    @unittest.expectedFailure
-    def test_no_paren(self):
-        self.assertEqual(max_integer(None), TypeError)
-
-    @unittest.expectedFailure
-    def test_minus(self):
-        self.assertEqual(max_integer(-[1, 2, 3, 4]), TypeError)
-
-    @unittest.expectedFailure
-    def test_one_digit(self):
-        self.assertEqual(max_integer([1]), TypeError)
-
-    @unittest.expectedFailure
-    def test_with_No_ints(self):
-        self.assertEqual(max_integer([1, None, 3, None]), TypeError)
-
-    @unittest.expectedFailure
-    def test_string_in_intlist(self):
-        self.assertEqual(max_integer([1, 2, 3, "hi"]), TypeError)
-
-    @unittest.expectedFailure
-    def test_string(self):
-        self.assertEqual(max_integer(["max", 1,2,3], TypeError)
+if __name__ == '__main__':
+        unittest.main()
