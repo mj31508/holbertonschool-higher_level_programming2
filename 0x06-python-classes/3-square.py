b143f1be06e852565e1c8c8f5cd4864eface42cb
Max <mj31508@gmail.com>
2017-06-08 23:36:06 -0400

    change

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,8 @@
 #!/bin/usr/python3
 
+
 class Square:
-    def __init__(self,size=0):
+    def __init__(self, size=0):
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
