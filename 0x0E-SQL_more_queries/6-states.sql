c7f90ca335f412b77a8a6cde0dbf9c7512a1fa8d
Max <mj31508@gmail.com>
2017-07-16 21:55:56 -0400

    script that creates a database and a table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- script that creat a database ad a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256), PRIMARY KEY(id));
