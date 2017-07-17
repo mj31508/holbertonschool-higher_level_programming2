759b7e700d1f0acb48ae96d1c71545a82d6bc4ff
Max <mj31508@gmail.com>
2017-07-17 01:06:28 -0400

    removal of syntax issues

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- script that lists all the cities in a database
 
 SELECT id, name FROM cities
-WHERE state_id IN ( SELECT id FROM states WHERE name = California)
+WHERE state_id IN (SELECT id FROM states WHERE name = California)
 ORDER BY cities.id ASC;
