2dd1af15b88402def77c12178c7181ad5342078e
Max <mj31508@gmail.com>
2017-07-17 01:03:59 -0400

    removed semi colon

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- script that lists all the cities in a database
-USE hbtn_0d_usa;
 
-SELECT id, name FROM cities;
+SELECT id, name FROM cities
 WHERE state_id IN ( SELECT id FROM states WHERE name = California)
 ORDER BY cities.id ASC;
