bf11e1570eadac6d88659fcc29f3ceb4f521e449
Max <mj31508@gmail.com>
2017-07-18 13:56:35 -0400

    fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- database dump
 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-       FROM tv_show_genres
-       JOIN tv_show_genres
+SELECT tv_genres.name AS genre
+       COUNT(tv_show_genres.genre_id) AS number_shows
+       FROM tv_show_genres JOIN tv_show_genres
        ON tv_genres.id = tv_show_genres.genre_id
        WHERE tv_show_genres.show_id IS NOT NULL
        GROUP BY tv_genres.name
