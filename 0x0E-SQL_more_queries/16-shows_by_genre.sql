490d0bef3a8465d0547fe90cbbf4070308cac2bf
Max <mj31508@gmail.com>
2017-07-18 00:59:06 -0400

    syntax fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- full list
-SELECT tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+SELECT tv_shows.title,tv_genres.name FROM tv_shows
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC
