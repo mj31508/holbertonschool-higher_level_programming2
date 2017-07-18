e09c698e81123fea171486abdba840652da1f435
Max <mj31508@gmail.com>
2017-07-18 01:00:43 -0400

    fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- full list
-SELECT tv_shows.title,tv_genres.name FROM tv_shows
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC
