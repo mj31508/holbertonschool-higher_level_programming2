14e89e34d835ed7c4b9ceeaae24c5e829c362c9e
Max <mj31508@gmail.com>
2017-08-11 19:17:15 -0400

    fixed pep8 issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 # selecting items from a db
 
-if __name__== "__main__":
+if __name__ == "__main__":
     import MySQLdb
     import sys
-    connect = MySQLdb.connect(user=sys.argv[1], host="localhost", port=3306,password=sys.argv[2], db=sys.argv[3])
+    connect = MySQLdb.connect(user=sys.argv[1], host="localhost",
+                              port=3306, password=sys.argv[2],
+                              db=sys.argv[3])
 
     now = connect.cursor()
     now.execute("SELECT * FROM states ORDER BY states.id ASC")
