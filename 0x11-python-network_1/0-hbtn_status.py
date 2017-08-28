6c20ef19e7fc6c98a5dcac422c868f17127f475c
Max <mj31508@gmail.com>
2017-08-28 17:11:21 -0400

    Python script that fetches a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Python script that fetches a url
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        url = response.read()
+        print("Body Response:\n"
+              "\t- type: {}\n"
+              "\t- content: {}\n"
+              "\t- utf content: {} "
+              .format(type(url), url, url.decode("utf-8")))
