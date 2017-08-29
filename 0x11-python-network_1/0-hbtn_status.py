9ab583e579cd54411634417735f7f97c96bbe184
Max <mj31508@gmail.com>
2017-08-29 13:21:43 -0400

    peer help fix/ forgot to add body response string

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         url = response.read()
-        print("Body Response")
+        print("Body Response:")
         print("\t- type: {}".format(type(url)))
         print("\t- content: {}".format(url))
         print("\t- utf8 content: {}".format(url.decode("utf-8")))
