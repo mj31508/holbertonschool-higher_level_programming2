a6440f573482d8c4360faafafbfa403e497de804
Max <mj31508@gmail.com>
2017-08-29 13:17:19 -0400

    peer help fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,6 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         url = response.read()
-        print("Body Response:\n"
-              t- type: {}\n"
-              "\t- content: {}\n"
-              "\t- utf content: {} "
-              .format(type(url), url, url.decode("utf-8")))
+        print("\t- type: {}".format(type(url)))
+        print("\t- content: {}".format(url))
+        print("\t- utf8 content: {}".format(url.decode("utf-8")))
