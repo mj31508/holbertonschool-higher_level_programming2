dfd956cf93d5d24d66d3aa9844c599d49aa58db2
Max <mj31508@gmail.com>
2017-08-30 14:19:56 -0400

    peer help

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 # script that takes in a URL and an email, sends a POST request
 
-import urlib.request
+import urllib.request
 import sys
 
-if __name__ == '__main__':
-    email_parse = urllib.parse.urlencode({"email": sys.argv[2]}).encode("utf-8")
-    url_request = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(url_request) as answer:
-        print (response.read())
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    email_dic = {"email": email}
+    print(email_dic)
+    data = urllib.parse.urlencode(email_dic)
+    data = data.encode('ascii')
+    with urllib.request.urlopen(url, data) as response:
+        read = response.read()
+        print(read.decode('utf-8'))
