a7282c8098ee44da643464fb1fe45a9c4f812036
Max <mj31508@gmail.com>
2017-09-20 15:03:03 -0400

    printing 3 lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// printing 3 lines
+
+let string = ['C', 'Python', 'Javscript'];
+let string2 = ['fun', 'cool', 'amazing'];
+
+for (let i = 0; i < string.length; i++) {
+  console.log(string[i] + ' is ' + string2[i]);
+}
