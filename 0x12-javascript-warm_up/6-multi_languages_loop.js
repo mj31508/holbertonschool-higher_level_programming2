62b357ed90b4618dbb6fa500f5c423e6e5c19787
Max <mj31508@gmail.com>
2017-09-20 15:05:26 -0400

    fixed error'

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -5,5 +5,5 @@ let string = ['C', 'Python', 'Javascript'];
 let string2 = ['fun', 'cool', 'amazing'];
 
 for (let i = 0; i < string.length; i++) {
-    console.log(string[i] + ' is ' +  string2[i])
+  console.log(string[i] + ' is ' + string2[i]);
 }
