50a1a32e6eb450dbaac01ea7e1923432c219c20d
Max <mj31508@gmail.com>
2017-09-20 00:22:02 -0400

    producing three lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// printing 3 lines
+
+const oneLines = 'C is fun\n';
+const twoLines = 'Python is cool\n';
+const threeLines = 'Javascript is amazing';
+console.log(oneLines + twoLines + threeLines);
