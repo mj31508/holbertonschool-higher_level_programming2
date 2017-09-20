fc965dd4d8c5aaf46f8b295076292d71742b3283
Max <mj31508@gmail.com>
2017-09-20 15:04:24 -0400

    fixed variable error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // printing 3 lines
 
-let string = ['C', 'Python', 'Javscript'];
+let string = ['C', 'Python', 'Javascript'];
 let string2 = ['fun', 'cool', 'amazing'];
 
 for (let i = 0; i < string.length; i++) {
-  console.log(string[i] + ' is ' + string2[i]);
+    console.log(string[i] + ' is ' +  string2[i])
 }
