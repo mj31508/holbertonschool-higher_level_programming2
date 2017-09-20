9c48611eab2d6b05f79fd4fcb2f3185f920ddee1
Max <mj31508@gmail.com>
2017-09-20 02:03:13 -0400

    fixed variables

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // print a message depending on the arg
 
 let args = process.argv;
-let num_args = args.slice(2);
-let arg_one = num_args[0];
+let NuMArgS = args.slice(2);
+let ArGOnE = NuMArgS[0];
 
-if (!arg_one) {console.log('No argument');} else {console.log(arg_one);}
+if (!ArGOnE) { console.log('No argument'); } else { console.log(ArGOnE); }
