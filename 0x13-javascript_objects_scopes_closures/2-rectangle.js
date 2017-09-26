f47db9fbafaf9a8efe4804f7dd9ead60acdf28bc
Max <mj31508@gmail.com>
2017-09-25 21:25:06 -0400

    checking for undefined in a javascript code

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
 exports.Rectangle = function Rectangle (w, h) {
-  if ((w !== undefined && w > 0) || (h !== undefined && h > 0)) {
-    this.width = w;
-    this.height = h;
-  }
+    if ((w === undefined && w < 0) && (h === undefined && h < 0))
+    {
+	this.width = w;
+	this.height = h;
+    }
+
 };
