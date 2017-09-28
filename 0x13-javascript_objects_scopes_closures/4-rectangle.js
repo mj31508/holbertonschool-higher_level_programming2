6cd18e86ef8d87c68b3bce516ae1218d4357f94b
Max <mj31508@gmail.com>
2017-09-27 21:10:24 -0400

    rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -4,23 +4,21 @@ exports.Rectangle = function Rectangle (w, h) {
   if ((w !== undefined && w > 0) && (h !== undefined && h > 0)) {
     this.width = w;
     this.height = h;
-
-    this.print = function () {
-      for (let x = 0; x < h; x++) {
-        console.log('X'.repeat(w));
-      }
-    };
-
-    this.rotate = function () {
-      let temp = w;
-      w = h;
-      h = temp;
-    };
-
-    this.double = function () {
-      w *= 2;
-      h *= 2;
-    };
-    console.log();
   }
+  this.print = function () {
+    for (let x = 0; x < this.height; x++) {
+      console.log('X'.repeat(this.width));
+    }
+  };
+
+  this.rotate = function () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  };
+
+  this.double = function () {
+    this.width *= 2;
+    this.height *= 2;
+  };
 };
